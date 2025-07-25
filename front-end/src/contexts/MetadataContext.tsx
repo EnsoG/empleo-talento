cb33e6c5dbedbc33dc9bd77da5821531a0fbfa5d
EnsoG <104644339+EnsoG@users.noreply.github.com>
2025-07-25 11:16:09 -0400

    Delete front-end directory

--- front-end/src/contexts/MetadataContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-import { Metadata } from "../types";
-
-interface MetadataContextType {
-    metadata: Metadata
-}
-
-export const MetadataContext = createContext<MetadataContextType | undefined>(undefined);
--- front-end/src/contexts/ModalContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, ReactNode } from "react";
-
-interface ModalContextType {
-    openedModal: boolean;
-    openModal: (content: ReactNode, title: string) => void;
-    closeModal: () => void;
-}
-
-export const ModalContext = createContext<ModalContextType | undefined>(undefined);
